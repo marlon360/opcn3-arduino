5c566344cc44698381888f4ac52aa8d3c24b2122
Marlon Lückert <m.lueckert@me.com>
2019-09-15 18:39:42 +0200

    configuration variables

--- src/OPCN3.cpp
+++ src/OPCN3.cpp
@@ -29,6 +29,10 @@ void OPCN3::initialize()
     begin();
     delay(1000);
     readSerialNumber();
+    delay(1000);
+    Serial.println("ConfigurationVariables");
+    readConfigurationVariables();
+    delay(1000);
     Serial.println("DACandPowerStatus");
     readDACandPowerStatus();
     delay(10000);
@@ -211,7 +215,8 @@ struct HistogramData OPCN3::readHistogramData()
     return histogramData;
 }
 
-bool OPCN3::resetHistogram() {
+bool OPCN3::resetHistogram()
+{
     HistogramData data = readHistogramData();
     return data.valid;
 }
@@ -221,10 +226,198 @@ struct SerialNumber OPCN3::readSerialNumber()
     SerialNumber serialNumber = sendCommand<SerialNumber>(0X10, 0X10, 60);
     Serial.print("Validity: ");
     Serial.println(serialNumber.valid);
-    for (int i = 0; i < 60; i++) {
+    for (int i = 0; i < 60; i++)
+    {
         Serial.print(serialNumber.serial[i]);
     }
     Serial.println("");
+}
+
+struct ConfigurationVariables OPCN3::readConfigurationVariables()
+{
+    ConfigurationVariables configurationVariables = sendCommand<ConfigurationVariables>(0X3C, 0X3C, 163);
+
+    Serial.print("Validity: ");
+    Serial.println(configurationVariables.valid);
+    Serial.println("Bin Boundries ADC");
+    Serial.print(configurationVariables.binBoundriesADC0);
+    Serial.print(" ");
+    Serial.print(configurationVariables.binBoundriesADC1);
+    Serial.print(" ");
+    Serial.print(configurationVariables.binBoundriesADC2);
+    Serial.print(" ");
+    Serial.print(configurationVariables.binBoundriesADC3);
+    Serial.print(" ");
+    Serial.print(configurationVariables.binBoundriesADC4);
+    Serial.print(" ");
+    Serial.print(configurationVariables.binBoundriesADC5);
+    Serial.print(" ");
+    Serial.print(configurationVariables.binBoundriesADC6);
+    Serial.print(" ");
+    Serial.print(configurationVariables.binBoundriesADC7);
+    Serial.print(" ");
+    Serial.print(configurationVariables.binBoundriesADC8);
+    Serial.print(" ");
+    Serial.print(configurationVariables.binBoundriesADC9);
+    Serial.println(" ");
+    Serial.print(configurationVariables.binBoundriesADC10);
+    Serial.print(" ");
+    Serial.print(configurationVariables.binBoundriesADC11);
+    Serial.print(" ");
+    Serial.print(configurationVariables.binBoundriesADC12);
+    Serial.print(" ");
+    Serial.print(configurationVariables.binBoundriesADC13);
+    Serial.print(" ");
+    Serial.print(configurationVariables.binBoundriesADC14);
+    Serial.print(" ");
+    Serial.print(configurationVariables.binBoundriesADC15);
+    Serial.print(" ");
+    Serial.print(configurationVariables.binBoundriesADC16);
+    Serial.print(" ");
+    Serial.print(configurationVariables.binBoundriesADC17);
+    Serial.print(" ");
+    Serial.print(configurationVariables.binBoundriesADC18);
+    Serial.print(" ");
+    Serial.print(configurationVariables.binBoundriesADC19);
+    Serial.println(" ");
+    Serial.print(configurationVariables.binBoundriesADC20);
+    Serial.print(" ");
+    Serial.print(configurationVariables.binBoundriesADC21);
+    Serial.print(" ");
+    Serial.print(configurationVariables.binBoundriesADC22);
+    Serial.print(" ");
+    Serial.print(configurationVariables.binBoundriesADC23);
+    Serial.print(" ");
+    Serial.print(configurationVariables.binBoundriesADC24);
+    Serial.println(" ");
+
+    Serial.println("-------------------------------------------------");
+    Serial.println("Bin Boundries Diametors");
+    Serial.print(configurationVariables.binBoundriesDiametor0);
+    Serial.print(" ");
+    Serial.print(configurationVariables.binBoundriesDiametor1);
+    Serial.print(" ");
+    Serial.print(configurationVariables.binBoundriesDiametor2);
+    Serial.print(" ");
+    Serial.print(configurationVariables.binBoundriesDiametor3);
+    Serial.print(" ");
+    Serial.print(configurationVariables.binBoundriesDiametor4);
+    Serial.print(" ");
+    Serial.print(configurationVariables.binBoundriesDiametor5);
+    Serial.print(" ");
+    Serial.print(configurationVariables.binBoundriesDiametor6);
+    Serial.print(" ");
+    Serial.print(configurationVariables.binBoundriesDiametor7);
+    Serial.print(" ");
+    Serial.print(configurationVariables.binBoundriesDiametor8);
+    Serial.print(" ");
+    Serial.print(configurationVariables.binBoundriesDiametor9);
+    Serial.println(" ");
+    Serial.print(configurationVariables.binBoundriesDiametor10);
+    Serial.print(" ");
+    Serial.print(configurationVariables.binBoundriesDiametor11);
+    Serial.print(" ");
+    Serial.print(configurationVariables.binBoundriesDiametor12);
+    Serial.print(" ");
+    Serial.print(configurationVariables.binBoundriesDiametor13);
+    Serial.print(" ");
+    Serial.print(configurationVariables.binBoundriesDiametor14);
+    Serial.print(" ");
+    Serial.print(configurationVariables.binBoundriesDiametor15);
+    Serial.print(" ");
+    Serial.print(configurationVariables.binBoundriesDiametor16);
+    Serial.print(" ");
+    Serial.print(configurationVariables.binBoundriesDiametor17);
+    Serial.print(" ");
+    Serial.print(configurationVariables.binBoundriesDiametor18);
+    Serial.print(" ");
+    Serial.print(configurationVariables.binBoundriesDiametor19);
+    Serial.println(" ");
+    Serial.print(configurationVariables.binBoundriesDiametor20);
+    Serial.print(" ");
+    Serial.print(configurationVariables.binBoundriesDiametor21);
+    Serial.print(" ");
+    Serial.print(configurationVariables.binBoundriesDiametor22);
+    Serial.print(" ");
+    Serial.print(configurationVariables.binBoundriesDiametor23);
+    Serial.print(" ");
+    Serial.print(configurationVariables.binBoundriesDiametor24);
+    Serial.println(" ");
+
+    Serial.println("-------------------------------------------------");
+    Serial.println("Bin Weights");
+    Serial.print(configurationVariables.binWeightings0);
+    Serial.print(" ");
+    Serial.print(configurationVariables.binWeightings1);
+    Serial.print(" ");
+    Serial.print(configurationVariables.binWeightings2);
+    Serial.print(" ");
+    Serial.print(configurationVariables.binWeightings3);
+    Serial.print(" ");
+    Serial.print(configurationVariables.binWeightings4);
+    Serial.print(" ");
+    Serial.print(configurationVariables.binWeightings5);
+    Serial.print(" ");
+    Serial.print(configurationVariables.binWeightings6);
+    Serial.print(" ");
+    Serial.print(configurationVariables.binWeightings7);
+    Serial.print(" ");
+    Serial.print(configurationVariables.binWeightings8);
+    Serial.print(" ");
+    Serial.print(configurationVariables.binWeightings9);
+    Serial.println(" ");
+    Serial.print(configurationVariables.binWeightings10);
+    Serial.print(" ");
+    Serial.print(configurationVariables.binWeightings11);
+    Serial.print(" ");
+    Serial.print(configurationVariables.binWeightings12);
+    Serial.print(" ");
+    Serial.print(configurationVariables.binWeightings13);
+    Serial.print(" ");
+    Serial.print(configurationVariables.binWeightings14);
+    Serial.print(" ");
+    Serial.print(configurationVariables.binWeightings15);
+    Serial.print(" ");
+    Serial.print(configurationVariables.binWeightings16);
+    Serial.print(" ");
+    Serial.print(configurationVariables.binWeightings17);
+    Serial.print(" ");
+    Serial.print(configurationVariables.binWeightings18);
+    Serial.print(" ");
+    Serial.print(configurationVariables.binWeightings19);
+    Serial.println(" ");
+    Serial.print(configurationVariables.binWeightings20);
+    Serial.print(" ");
+    Serial.print(configurationVariables.binWeightings21);
+    Serial.print(" ");
+    Serial.print(configurationVariables.binWeightings22);
+    Serial.print(" ");
+    Serial.print(configurationVariables.binWeightings23);
+    Serial.println(" ");
+    Serial.println("-------------------------------------------------");
+    Serial.println("PM Diametors");
+    Serial.print(configurationVariables.pmDiametorA);
+    Serial.print(" ");
+    Serial.print(configurationVariables.pmDiametorB);
+    Serial.print(" ");
+    Serial.print(configurationVariables.pmDiametorC);
+    Serial.println(" ");
+    Serial.println("-------------------------------------------------");
+    Serial.println("PM MSLNS");
+    Serial.print(configurationVariables.maximumTimeOfFlight);
+    Serial.print(" ");
+    Serial.print(configurationVariables.AMSamplingIntervalCount);
+    Serial.print(" ");
+    Serial.print(configurationVariables.AMIdleIntervalCount);
+    Serial.print(" ");
+    Serial.print(configurationVariables.AMMaxDataArraysInFile);
+    Serial.print(" ");
+    Serial.print(configurationVariables.AMOnlySavePMData);
+    Serial.print(" ");
+    Serial.print(configurationVariables.AMFanOnInIdle);
+    Serial.print(" ");
+
+    return configurationVariables;
 }
 
 void OPCN3::beginTransfer()
@@ -242,8 +435,10 @@ void OPCN3::endTransfer()
 bool OPCN3::validate(byte arrayOne[], byte arrayTwo[], int size)
 {
     bool valid = true;
-    for (int i = 0; i < size; i++) {
-        if (arrayOne[i] != arrayTwo[i]) {
+    for (int i = 0; i < size; i++)
+    {
+        if (arrayOne[i] != arrayTwo[i])
+        {
             valid = false;
         }
     }
