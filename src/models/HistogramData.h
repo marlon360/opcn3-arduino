ddf6d6c11f08689ddd3874c42b80977ba26bc34e
Marlon Lückert <m.lueckert@me.com>
2019-09-21 14:45:05 +0200

    use arrays

--- src/models/HistogramData.h
+++ src/models/HistogramData.h
@@ -3,30 +3,7 @@
 struct HistogramData
 {
 
-    uint16_t binCount0;
-    uint16_t binCount1;
-    uint16_t binCount2;
-    uint16_t binCount3;
-    uint16_t binCount4;
-    uint16_t binCount5;
-    uint16_t binCount6;
-    uint16_t binCount7;
-    uint16_t binCount8;
-    uint16_t binCount9;
-    uint16_t binCount10;
-    uint16_t binCount11;
-    uint16_t binCount12;
-    uint16_t binCount13;
-    uint16_t binCount14;
-    uint16_t binCount15;
-    uint16_t binCount16;
-    uint16_t binCount17;
-    uint16_t binCount18;
-    uint16_t binCount19;
-    uint16_t binCount20;
-    uint16_t binCount21;
-    uint16_t binCount22;
-    uint16_t binCount23;
+    uint16_t binCounts[24];
 
     uint8_t bin1TimeToCross;
     uint8_t bin3TimeToCross;
